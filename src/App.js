6b0316d026a35a1027fea45eb52a6d960b640795
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-05-09 07:04:11 +0900

    lesson7を修了

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
+import { ChildArea } from "./ChildArea";
 import "./index.css";
 
 export default function App() {
+  console.log('App');
+
+  // ========================================
+  // stateを定義
+  // ========================================
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
+  const [open, setOpen] = useState(false)
+
+
+  // ========================================
+  // イベント関数を定義
+  // ========================================
+  const onClickCountUp = () => setCount(count + 1)
+  const onChangeText = (e) => setText(e.target.value)
+  const onClickOpen = () => setOpen(!open)
 
   return (
     <div className="App">
-      <h1>こんにちは、せかい</h1>
-      <h2>これから学習していきます</h2>
-      <h3> "type": "module"を追加</h3>
+      <p>{count}</p>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <br />
+
+      <input type="text" value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
 }
